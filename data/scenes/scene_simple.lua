1ea11f64fc49e09b3904c129f7fa93b15e3e8033
Yvens Rebouças Serpa <y.reboucasserpa@saxion.nl>
2024-12-08 20:35:42 +0100

    Try to force copying the data folder every build and load shaders from Lua

--- data/scenes/scene_simple.lua
+++ data/scenes/scene_simple.lua
@@ -1,2 +1,24 @@
+vertexShader = [[
+    #version 400
+    layout (location = 0) in vec3 vertex;
+    layout (location = 1) in vec3 normal;
+    layout (location = 2) in vec2 uv;
+    uniform mat4 mvpMatrix;
+    out vec4 nn;
+
+    void main() {
+      nn = vec4(normal, 1.0);
+      gl_Position = mvpMatrix * vec4(vertex, 1.0);
+    }
+]]
+
+fragmentShader = [[
+    #version 400
+    in vec4 nn;
+    out vec4 frag_colour;
+    void main() {
+      frag_colour = vec4(nn.xyz, 1.0);
+    }
+]]
+
 monkey = {file = "data/models/monkey.fbx",  pos = { 0.0, 0.5, -4.0}, sca = {1.0, 1.0, 1.0}, rot = {0.0,  0.0, 0.0}}
-nearPlane = -4.0
--- main.cpp
+++ main.cpp
@@ -11,44 +11,6 @@ int main() {
     constexpr int height = 600;
     core::YRenderer renderer(width, height, "YRenderer");
     renderer.changeClearColor(glm::vec4(1.0f, 0.2f, 0.3f, 1.0f));
-
-#pragma region test_scene
-    const auto vertexShader =
-            "#version 400\n"
-            "layout (location = 0) in vec3 vertex;"
-            "layout (location = 1) in vec3 normal;"
-            "layout (location = 2) in vec2 uv;"
-            "uniform mat4 mvpMatrix;"
-            "out vec4 nn;"
-            "void main() {"
-            "  nn = vec4(normal, 1.0);"
-            "  gl_Position = mvpMatrix * vec4(vertex, 1.0);"
-            "}";
-
-    const auto fragmentShader =
-            "#version 400\n"
-            "in vec4 nn;"
-            "out vec4 frag_colour;"
-            "void main() {"
-            "  frag_colour = vec4(nn.xyz, 1.0);"
-            "}";
-
-    const auto vertexProgram = core::YShader::generateShader(vertexShader, GL_VERTEX_SHADER);
-    const auto fragmentProgram = core::YShader::generateShader(fragmentShader, GL_FRAGMENT_SHADER);
-    const auto shaderProgram = core::YShader::generateProgram(vertexProgram, fragmentProgram);
-
-    const core::YCamera &camera = renderer.getCamera();
-
-    const GLint mvpMatrixUniform = glGetUniformLocation(shaderProgram, "mvpMatrix");
-
-    const glm::mat4 view = camera.getView();
-    const glm::mat4 projection = glm::perspective(glm::radians(camera.getZoom()),
-                                                  static_cast<float>(width) / static_cast<float>(height), 0.1f,
-                                                  1000.0f);
-    auto modelMatrix = glm::mat4(1.0f);
-
-    const glm::mat4 viewProjection = projection * view;
-
     const LuaHandler luaHandler;
     if (luaHandler.openFile("data/scenes/scene_simple.lua")) {
         printf("Successfully loaded scene_simple.lua\n");
@@ -56,9 +18,22 @@ int main() {
         printf("Failed to load scene_simple.lua\n");
     }
 
-    const auto value = luaHandler.getGlobalNumber("nearPlane");
-    printf("%f\n", value);
+#pragma region test_scene
+    const auto vertexShader = luaHandler.getGlobalString("vertexShader");
+    const auto fragmentShader = luaHandler.getGlobalString("fragmentShader");
+    const auto vertexProgram = core::YShader::generateShader(vertexShader, GL_VERTEX_SHADER);
+    const auto fragmentProgram = core::YShader::generateShader(fragmentShader, GL_FRAGMENT_SHADER);
+    const auto shaderProgram = core::YShader::generateProgram(vertexProgram, fragmentProgram);
 
+    const core::YCamera &camera = renderer.getCamera();
+    const GLint mvpMatrixUniform = glGetUniformLocation(shaderProgram, "mvpMatrix");
+    const glm::mat4 view = camera.getView();
+    const glm::mat4 projection = glm::perspective(glm::radians(camera.getZoom()),
+                                                  static_cast<float>(width) / static_cast<float>(height), 0.1f,
+                                                  1000.0f);
+    auto modelMatrix = glm::mat4(1.0f);
+
+    const glm::mat4 viewProjection = projection * view;
     const core::YModel monkey = utils::YLuaHelper::loadModelFromTable("monkey", luaHandler);
 
 #pragma endregion
