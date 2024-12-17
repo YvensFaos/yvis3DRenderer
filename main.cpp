b11c48e5a946b18a5d4fd698b329903560e3043f
Yvens Rebouças Serpa <y.reboucasserpa@saxion.nl>
2024-12-17 21:43:06 +0100

    Clear skybox_scene.lua and remove it from main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include "core/yrenderer.h"
 #include "LuaHandler/luahandler.hpp"
+
+//Scenes
+#include "scenes/yfogscene.h"
 #include "scenes/yskyboxscene.h"
+
 int main() {
     constexpr int width = 800;
     constexpr int height = 600;
     core::YRenderer renderer(width, height, "YRenderer");
 
-    scenes::YSkyBoxScene skyBoxScene(renderer, "data/scenes/skybox_scene.lua", "skybox", 800, 800);
+    // scenes::YSkyBoxScene skyBoxScene(renderer, "data/scenes/skybox_scene.lua", "skybox", width, height);
+    // scenes::YFogScene fogScene(renderer, "data/scenes/fog_scene.lua", width, height);
     do {
-        skyBoxScene.render();
+        //scene.render();
     } while (renderer.isRunning());
 }
